810b8cb8dd5ea8df8d7914fa16d961e077c29af6
Alfonso AG <alfareiza@gmail.com>
2020-10-04 17:21:22 -0300

    Class that has default values of the game

---
+++ game/settings.py
@@ -0,0 +1,6 @@
+class Settings():
+    def __init__(self):
+        """A class to store all the configurations of the Alien Invasion"""
+        self.screen_width = 900
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
