2e13705f0906743cce6eb3c982336268ac10da5d
Alfonso AG <alfareiza@gmail.com>
2020-10-04 17:12:53 -0300

    background color for the inicial screen

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 
+
 def run_game():
     """
     Starts the game
@@ -9,13 +10,16 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((900, 600))
     pygame.display.set_caption("Alien Invasion")
-
+    bg_color = (230, 230, 230)  # Background Color
     # Starts main loop of the game
     while True:
-        for event in pygame.event.get():
+        for event in pygame.event.get():  # Observes keyboard and mouse events
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        pygame.display.flip()
+        screen.fill(bg_color)  # Redesign the screen and each passage through the while
+        pygame.display.flip()  # Leave the most recent screen visible
+
 
 run_game()
+9
