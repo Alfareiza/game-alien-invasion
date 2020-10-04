a9b7e8b210c48e946c061a37666a756d442000cc
Alfonso AG <alfareiza@gmail.com>
2020-10-04 17:21:50 -0300

    File adaptation to take data from settings.py

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from game.settings import Settings
 
 
 def run_game():
@@ -8,18 +9,19 @@ def run_game():
     :return:
     """
     pygame.init()
-    screen = pygame.display.set_mode((900, 600))
+
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (230, 230, 230)  # Background Color
     # Starts main loop of the game
     while True:
         for event in pygame.event.get():  # Observes keyboard and mouse events
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill(bg_color)  # Redesign the screen and each passage through the while
+        screen.fill(ai_settings.bg_color)  # Redesign the screen and each passage through the while
         pygame.display.flip()  # Leave the most recent screen visible
 
 
 run_game()
-9
