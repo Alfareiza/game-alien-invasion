6cc0e03f016e0d5817a95f6d4d9ecd4ebf83e8a8
Alfonso AG <alfareiza@gmail.com>
2020-10-05 19:58:06 -0300

    adding a ship to the game and changing the background

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from game.settings import Settings
+from game.ship import Ship
 
 
 def run_game():
@@ -12,15 +13,22 @@ def run_game():
 
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
-
     pygame.display.set_caption("Alien Invasion")
+
+    # Create a spaceship
+    ship = Ship(screen)
+
     # Starts main loop of the game
     while True:
         for event in pygame.event.get():  # Observes keyboard and mouse events
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill(ai_settings.bg_color)  # Redesign the screen and each passage through the while
+        # Redesign the screen and each passage through the while
+        # screen.fill(ai_settings.bg_color)
+        screen.blit(ai_settings.bg_image, [0, 0])
+        ship.blitme()
+
         pygame.display.flip()  # Leave the most recent screen visible
 
 
