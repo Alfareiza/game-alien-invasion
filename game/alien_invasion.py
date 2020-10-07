0a4aaea57d7f3f8730ca33886e16608ff787a11f
Alfonso AG <alfareiza@gmail.com>
2020-10-07 18:24:45 -0300

    calling update_bullet() to delete bullets out of screen

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -27,7 +27,8 @@ def run_game():
     while True:
         game_functions.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
+        game_functions.update_bullets(bullets)
         game_functions.update_screen(ai_settings, screen, ship, bullets)
+
 
 run_game()
