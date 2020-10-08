81644a0daa1ae1490e5bfdadc460b18ad06ab0dc
Alfonso AG <alfareiza@gmail.com>
2020-10-08 09:27:10 -0300

    expanding area of game to 1.100px

--- game/settings.py
+++ game/settings.py
@@ -4,7 +4,7 @@ import pygame
 class Settings():
     def __init__(self):
         """A class to store all the configurations of the Alien Invasion"""
-        self.screen_width = 900
+        self.screen_width = 1100
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.bg_image = pygame.image.load('img/bg.png')
