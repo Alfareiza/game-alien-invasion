eae94e4cb74b06f8cc7fb7459824e5303146659a
alfonso <@lfareiza1989>
2020-10-10 18:28:47 -0300

    Added flag game_active

--- game/game_stats.py
+++ game/game_stats.py
@@ -2,6 +2,7 @@ class GameStats():
     def __init__(self, ai_settings):
         self.ai_settings = ai_settings
         self.reset_stats()
+        self.game_active = True
 
     def reset_stats(self):
         """Initializes statistical data that may change during the game"""
