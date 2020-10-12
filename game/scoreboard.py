8726df7b716b2c04a8b8505bc0e8b21ad906f447
alfonso <@lfareiza1989>
2020-10-12 18:28:19 -0300

    Calculating high score

--- game/scoreboard.py
+++ game/scoreboard.py
@@ -33,6 +33,15 @@ class Scoreboard():
     def show_score(self):
         """Desenha a pontuação na tela"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
 
     def prep_high_score(self):
-        pass
+        """Transforma a pontuação máxima em uma imagem renderizada"""
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color)
+
+        # Centraliza a pontuação máxima na parte superior da tela
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
