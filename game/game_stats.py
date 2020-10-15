a405d6d8d5b09842d897fcef84a14dfbe577d22b
Alfonso AG <alfareiza@gmail.com>
2020-10-15 19:08:30 -0300

    New Attribute "level"

--- game/game_stats.py
+++ game/game_stats.py
@@ -9,3 +9,4 @@ class GameStats():
         """Initializes statistical data that may change during the game"""
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
+        self.level = 1
